5396c07192999295d8fb4937a2642859f0152b76
uralteszinn <uralteszinn@zoho.com>
2017-01-02 12:47:39 +0100

    Revert "Added output of IPs per Interface on right click"
    This reverts commit a1b68ca7a8ca01f61b939d5631fa21cc8ef1f50e.

--- rclib/Widgets/Internet/bindings.lua
+++ rclib/Widgets/Internet/bindings.lua
@@ -1,24 +1,5 @@
 internetTextbox:buttons(awful.util.table.join(
   awful.button({ },1, function() 
       hadInternet = not hadInternet
-    end),
-  awful.button({ },3, function() 
-
-      ips = getInternalIPs(interfaces)
- 
-      res = ""
-      for iface,ip in pairs(ips) do 
-          res = res .. "\n" ..  iface .. ": \n" .. ip
-      end
-
-      naughty.notify({
-                       screen    = s,
-                       title     = "Internet IPs",
-                       text      = res,
-                       icon      = ico_path .. "internet.png",
-                       icon_size = 30,
-                       preset    = naughty.config.presets.normal,
-                       timeout   = 30,
-                    })
     end)
 ))
