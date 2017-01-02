f25f93083d71084886f04c81d5833298c3f2a988
uralteszinn <uralteszinn@zoho.com>
2017-01-02 11:57:23 +0100

    Added output of IPs per Interface on right click

--- rclib/variables.lua
+++ rclib/variables.lua
@@ -3,12 +3,12 @@ modkey            = "Mod4"
 ico_path          = lib_path .. "Icons/"
 wlp_path          = lib_path .. "Wallpapers/"
 
-terminal          = "urxvtc"
+terminal          = "xterm"
 browser           = "firefox"
 pdfviewer         = "evince"
-networkmanager    = "wicd-client -n"
+networkmanager    = "" --wicd,networkmanager,connman,netctl,wpa_gui
 filemanager       = "nautilus"
-applauncher       = "dmenu_run"
+applauncher       = "" --dmenu
 editor            = os.getenv("EDITOR") or "gvim"
 
 editor_cmd        = terminal .. " -e " .. editor
@@ -17,5 +17,5 @@ suspend_cmd       = "systemctl suspend"
 poweroff_cmd      = "systemctl poweroff"
 reboot_cmd        = "systemctl reboot"
 
-pacman_update_cmd = "urxvtc -hold -e zsh -c \"sudo pacman -Syyu\""
+--pacman_update_cmd = "urxvtc -hold -e zsh -c \"sudo pacman -Syyu\""
 
